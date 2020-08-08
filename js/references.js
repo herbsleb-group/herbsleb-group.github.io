14bf0975b6e2cf58704985f8de7eafa1aaf4f4d6
Alexander Nolte <anolte@cmu.edu>
2020-08-08 19:24:24 +0300

    changed to more flexible reference management

---
+++ js/references.js
@@ -0,0 +1,430 @@
+// Reference management
+
+var references = [{
+	'id': 'intGoal',
+	'type': 'internal',
+	'text': 'Goal',
+	'link': '1'
+}, {
+	'id': 'intTheme',
+	'type': 'internal',
+	'text': 'Theme',
+	'link': '2'
+}, {
+	'id': 'intCompetition',
+	'type': 'internal',
+	'text': 'Competition / cooperation',
+	'link': '3'
+}, {
+	'id': 'intStakeholder',
+	'type': 'internal',
+	'text': 'Stakeholder involvement',
+	'link': '4'
+}, {
+	'id': 'intRecruitment',
+	'type': 'internal',
+	'text': 'Participant recruitment',
+	'link': '5'
+}, {
+	'id': 'intPreparation',
+	'type': 'internal',
+	'text': 'Specialized preparation',
+	'link': '6'
+}, {
+	'id': 'intDuration',
+	'type': 'internal',
+	'text': 'Duration / breaks',
+	'link': '7'
+}, {
+	'id': 'intIdeation',
+	'type': 'internal',
+	'text': 'Ideation',
+	'link': '8'
+}, {
+	'id': 'intTeam',
+	'type': 'internal',
+	'text': 'Team formation',
+	'link': '9'
+}, {
+	'id': 'intAgenda',
+	'type': 'internal',
+	'text': 'Agenda',
+	'link': '10'
+}, {
+	'id': 'intMentor',
+	'type': 'internal',
+	'text': 'Mentoring',
+	'link': '11'
+}, {
+	'id': 'intContinuity',
+	'type': 'internal',
+	'text': 'Continuity planning',
+	'link': '12'
+}, {
+	'id': 'filippova2017from',
+	'type': 'pub',
+	'text': 'Filippova, A., Trainer, E., & Herbsleb, J. D. (2017). From diversity by numbers to diversity as process: supporting inclusiveness in software development teams with brainstorming. In <i>2017 IEEE/ACM 39th International Conference on Software Engineering (ICSE)</i> (pp. 152-163). IEEE.',
+	'link': 'Filippova-ICSE-2017.pdf'
+}, {
+	'id': 'nolte2018you',
+	'type': 'pub',
+	'text': 'Nolte, A., Pe-Than, E. P. P., Filippova, A., Bird, C., Scallen, S., & Herbsleb, J. D. (2018). You Hacked and Now What? - Exploring Outcomes of a Corporate Hackathon. <i>Proc. ACM Hum.-Comput. Interact. 2</i>, CSCW, 129:1-129:23.',
+	'link': 'Nolte-CSCW-2018.pdf'
+}, {
+	'id': 'medina2019does',
+	'type': 'pub',
+	'text': 'Medina Angarita, M. A., & Nolte, A. (2019). Does it matter why we hack? - Exploring the impact of goal alignment in hackathons. In <i>Proceedings of 17th European Conference on Computer-Supported Cooperative Work</i>.',
+	'link': 'Medina-ECSCW-2019.pdf'
+}, {
+	'id': 'nolte2019touched',
+	'type': 'pub',
+	'text': 'Nolte, A. (2019). Touched by the Hackathon: A Study on the Connection between Hackathon Participants and Start-Up Founders. In <i>International Workshop on Software-intensive Business 2019: Start-ups, Ecosystems and Platforms (IWSiB 2019)</i> (pp. 31-36).',
+	'link': 'Nolte-SIGSOFT-workshop-2019.pdf'
+}, {
+	'id': 'pe2019understanding',
+	'type': 'pub',
+	'text': 'Pe-Than, E. P. P., & Herbsleb, J.D. (2019). Understanding Hackathons for Science: Collaboration, Affordances, and Outcomes. In <i>International Conference on Information (pp. 27-37)</i>.',
+	'link': 'Pethan-iConference-2019.pdf'
+}, {
+	'id': 'pe2019designing',
+	'type': 'pub',
+	'text': 'Pe-Than, E. P. P., Nolte, A., Filippova, A., Bird, C., Scallen, S. & Herbsleb, J. D. (2019). Designing Corporate Hackathons With a Purpose. <i>IEEE Software 36</i>, 1, 15-22.',
+	'link': 'Pethan-IEEESW-2019.pdf'
+}, {
+	'id': 'nolte2020mentor',
+	'type': 'pub',
+	'text': 'Nolte, A., Hayden, L. B., & Herbsleb, J. D. (2020). How to Support Newcomers in Scientific Hackathons - An Action Research Study on Expert Mentoring. <i>Proc. ACM Hum.-Comput. Interact. 4</i>, CSCW1, Article 25 (May 2020), 23 pages.',
+	'link': 'Nolte-CSCW-2020.pdf'
+}, {
+	'id': 'pe2020corporate',
+	'type': 'pub',
+	'text': 'Pe-Than, E. P. P., Nolte, A., Filippova, A., Bird, C., Scallen, S., & Herbsleb, J. D. (2020). Corporate Hackathons, How and Why? A Multiple Case Study of Motivation, Project Proposal and Selection, Goal Setting, Coordination, and Project Continuation. <i>Human-Computer Interaction</i>.',
+	'link': 'Pethan-HCI-2020.pdf'
+}, {
+	'id': 'playbook',
+	'type': 'pub',
+	'text': 'Hackathon playbook',
+	'link': 'hackathon-playbook-external.pdf'
+}, {
+	'id': 'zapico2013hacking',
+	'type': 'pubnopdf',
+	'text': 'Zapico, J. L., Pargman, D., Ebner, H., & Eriksson, E. (2013). Hacking sustainability: Broadening participation through green hackathons. In <i>Fourth International Symposium on End-User Development</i>.'
+}, {
+	'id': 'flow',
+	'type': 'pubnopdf',
+	'text': 'Csikszentmihalyi, M. (1997). Flow and the psychology of discovery and invention. HarperPerennial, New York.'
+}, {
+	'id': 'greatmentor',
+	'type': 'external',
+	'text': 'https://www.forbes.com/sites/carriekerpen/2018/06/18/5-things-great-mentors-do/',
+	'link': 'https://www.forbes.com/sites/carriekerpen/2018/06/18/5-things-great-mentors-do/'
+}, {
+	'id': 'perfectmentor',
+	'type': 'external',
+	'text': 'https://www.perfektio.fi/en/blog/how-to-be-a-mentor',
+	'link': 'https://www.perfektio.fi/en/blog/how-to-be-a-mentor'
+}, {
+	'id': 'g48mentor',
+	'type': 'external',
+	'text': 'https://garage48.org/blog/how-to-mentor-teams-at-a-hackathon',
+	'link': 'https://garage48.org/blog/how-to-mentor-teams-at-a-hackathon'
+}, {
+	'id': 'g48cyber',
+	'type': 'external',
+	'text': 'http://garage48.org/events/garage48-cyber-security-2019',
+	'link': 'http://garage48.org/events/garage48-cyber-security-2019'
+}, {
+	'id': 'g48how',
+	'type': 'external',
+	'text': 'http://garage48.org/how-it-works',
+	'link': 'http://garage48.org/how-it-works'
+}, {
+	'id': 'startupsfromhackathons',
+	'type': 'external',
+	'text': 'https://www.valuer.ai/blog/50-startups-that-came-from-hackathons',
+	'link': 'https://www.valuer.ai/blog/50-startups-that-came-from-hackathons'
+}, {
+	'id': 'woc',
+	'type': 'external',
+	'text': 'https://github.com/woc-hack',
+	'link': 'https://github.com/woc-hack'
+}, {
+	'id': 'woctutorial',
+	'type': 'external',
+	'text': 'https://github.com/woc-hack/tutorial',
+	'link': 'https://github.com/woc-hack/tutorial'
+}, {
+	'id': 'wocschedule',
+	'type': 'external',
+	'text': 'https://github.com/woc-hack/schedule',
+	'link': 'https://github.com/woc-hack/schedule'
+}, {
+	'id': 'sgci2019',
+	'type': 'external',
+	'text': 'https://sciencegateways.org/web/wd/hackathon-2019',
+	'link': 'https://sciencegateways.org/web/wd/hackathon-2019'
+}, {
+	'id': 'brainhack',
+	'type': 'external',
+	'text': 'https://ohbm.github.io/hackathon2018/',
+	'link': 'https://ohbm.github.io/hackathon2018/'
+}, {
+	'id': 'icebreaker',
+	'type': 'external',
+	'text': 'https://www.tinypulse.com/blog/sk-work-icebreaker-games',
+	'link': 'https://www.tinypulse.com/blog/sk-work-icebreaker-games'
+}, {
+	'id': 'nofun',
+	'type': 'external',
+	'text': 'https://medium.com/@sathishvj/please-dont-organize-fun-activities-at-hackathons-a3333f0bbc2c',
+	'link': 'https://medium.com/@sathishvj/please-dont-organize-fun-activities-at-hackathons-a3333f0bbc2c'
+}, {
+	'id': 'hackbox',
+	'type': 'external',
+	'text': 'https://formidable.com/work/hackbox/',
+	'link': 'https://formidable.com/work/hackbox/'
+}, {
+	'id': 'steelhacks',
+	'type': 'external',
+	'text': 'http://steelhacks.com/',
+	'link': 'http://steelhacks.com/'
+}, {
+	'id': 'dayofcivichacking',
+	'type': 'external',
+	'text': 'https://www.codeforamerica.org/events/national-day-of-civic-hacking-2019',
+	'link': 'https://www.codeforamerica.org/events/national-day-of-civic-hacking-2019'
+}, {
+	'id': 'greenhackathon',
+	'type': 'external',
+	'text': 'http://www.greenhackathon.com/',
+	'link': 'http://www.greenhackathon.com/'
+}, {
+	'id': 'brainstorm',
+	'type': 'external',
+	'text': 'https://www.wikihow.com/Brainstorm',
+	'link': 'https://www.wikihow.com/Brainstorm'
+}, {
+	'id': 'brainstormquestion',
+	'type': 'external',
+	'text': 'https://medium.com/brainfuel/7-tricks-to-build-the-perfect-brainstorming-question-f74a6ddc8a78',
+	'link': 'https://medium.com/brainfuel/7-tricks-to-build-the-perfect-brainstorming-question-f74a6ddc8a78'
+}, {
+	'id': 'oneweek',
+	'type': 'external',
+	'text': 'https://news.microsoft.com/life/one-week-microsoftlife/',
+	'link': 'https://news.microsoft.com/life/one-week-microsoftlife/'
+}, {
+	'id': 'chihacknight',
+	'type': 'external',
+	'text': 'https://chihacknight.org/',
+	'link': 'https://chihacknight.org/'
+}, {
+	'id': 'chihacknightprojects',
+	'type': 'external',
+	'text': 'https://chihacknight.org/projects.html',
+	'link': 'https://chihacknight.org/projects.html'
+}, {
+	'id': 'codecademy',
+	'type': 'external',
+	'text': 'https://www.codecademy.com/learn/learn-python-3',
+	'link': 'https://www.codecademy.com/learn/learn-python-3'
+}, {
+	'id': 'astrohackweek',
+	'type': 'external',
+	'text': 'http://astrohackweek.org/2020/',
+	'link': 'http://astrohackweek.org/2020/'
+}, {
+	'id': 'geohackweek',
+	'type': 'external',
+	'text': 'https://geohackweek.github.io/',
+	'link': 'https://geohackweek.github.io/'
+}, {
+	'id': 'hackweektoolkit',
+	'type': 'external',
+	'text': 'https://uwescience.github.io/HackWeek-Toolkit/',
+	'link': 'https://uwescience.github.io/HackWeek-Toolkit/'
+}, {
+	'id': 'hackweektoolkitrecruit',
+	'type': 'external',
+	'text': 'https://uwescience.github.io/HackWeek-Toolkit/#Objectives/Objectives-and-Goals/#target-audience-and-scoping-to-specific-communities',
+	'link': 'https://uwescience.github.io/HackWeek-Toolkit/#Objectives/Objectives-and-Goals/#target-audience-and-scoping-to-specific-communities'
+}, {
+	'id': 'hackhpc',
+	'type': 'external',
+	'text': 'http://hackhpc.org/',
+	'link': 'http://hackhpc.org/'
+}, {
+	'id': 'mlhmarketing',
+	'type': 'external',
+	'text': 'https://guide.mlh.io/digital-hackathons/marketing-your-event/how-to-promote-your-event',
+	'link': 'https://guide.mlh.io/digital-hackathons/marketing-your-event/how-to-promote-your-event'
+}, {
+	'id': 'mlhsponsors',
+	'type': 'external',
+	'text': 'https://guide.mlh.io/digital-hackathons/getting-sponsorship',
+	'link': 'https://guide.mlh.io/digital-hackathons/getting-sponsorship'
+}, {
+	'id': 'mlhjudging',
+	'type': 'external',
+	'text': 'https://guide.mlh.io/digital-hackathons/judging-and-submissions',
+	'link': 'https://guide.mlh.io/digital-hackathons/judging-and-submissions'
+}, {
+	'id': 'mlhprizes',
+	'type': 'external',
+	'text': 'https://guide.mlh.io/digital-hackathons/event-logistics/ordering-swags-and-prizes',
+	'link': 'https://guide.mlh.io/digital-hackathons/event-logistics/ordering-swags-and-prizes'
+}, {
+	'id': 'sheinnovates',
+	'type': 'external',
+	'text': 'http://sheinnovates.us/',
+	'link': 'http://sheinnovates.us/'
+}, {
+	'id': 'entrofy',
+	'type': 'external',
+	'text': 'https://github.com/dhuppenkothen/entrofy',
+	'link': 'https://github.com/dhuppenkothen/entrofy'
+}, {
+	'id': 'stakeholderanalysis',
+	'type': 'external',
+	'text': 'https://medium.com/the-value-maximizers/creating-a-stakeholder-analysis-how-do-you-do-that-b2b8198ca6c',
+	'link': 'https://medium.com/the-value-maximizers/creating-a-stakeholder-analysis-how-do-you-do-that-b2b8198ca6c'
+}, {
+	'id': 'codeforpgh',
+	'type': 'external',
+	'text': 'https://www.meetup.com/codeforpgh/events/268968230/',
+	'link': 'https://www.meetup.com/codeforpgh/events/268968230/'
+}, {
+	'id': 'biohack',
+	'type': 'external',
+	'text': 'http://hackathon.dbcls.jp/',
+	'link': 'http://hackathon.dbcls.jp/'
+}, {
+	'id': 'debcamp',
+	'type': 'external',
+	'text': 'https://wiki.debian.org/DebCamp',
+	'link': 'https://wiki.debian.org/DebCamp'
+}, {
+	'id': 'prizes',
+	'type': 'external',
+	'text': 'https://news.mlh.io/are-hackathon-prizes-the-worst-thing-since-moldy-sliced-bread-04-18-2014',
+	'link': 'https://news.mlh.io/are-hackathon-prizes-the-worst-thing-since-moldy-sliced-bread-04-18-2014'
+}, {
+	'id': 'globalhack',
+	'type': 'external',
+	'text': 'https://theglobalhack.com/',
+	'link': 'https://theglobalhack.com/'
+}, {
+	'id': 'openbio',
+	'type': 'external',
+	'text': 'https://www.open-bio.org/',
+	'link': 'https://www.open-bio.org/'
+}];
+
+var goalReferences = ['intRecruitment', 'intStakeholder', 'medina2019does', 'filippova2017from', 'intIdeation', 'intDuration', 'intAgenda', 'g48cyber', 'pe2019understanding', 'globalhack', 'nolte2020mentor', 'intMentor', 'astrohackweek', 'woc', 'intContinuity', 'pe2020corporate'];
+var themeReferences = ['intGoal', 'intStakeholder', 'globalhack', 'woc', 'g48cyber', 'astrohackweek', 'geohackweek', 'openbio', 'brainhack', 'zapico2013hacking', 'hackhpc', 'oneweek', 'pe2019understanding'];
+var competitionReferences = ['intStakeholder', 'intRecruitment', 'intGoal', 'intTheme', 'biohack', 'debcamp', 'pe2019understanding', 'mlhjudging', 'playbook', 'nolte2018you', 'oneweek', 'prizes', 'mlhprizes'];
+var stakeholderReferences = ['intTheme', 'intContinuity', 'stakeholderanalysis', 'steelhacks', 'g48cyber', 'nolte2018you', 'chihacknightprojects', 'pe2019understanding', 'mlhsponsors', 'intMentor', 'nolte2020mentor', 'codeforpgh', 'oneweek', 'intGoal'];
+var recruitmentReferences = ['intGoal', 'intTheme', 'astrohackweek', 'geohackweek', 'mlhmarketing', 'intStakeholder', 'oneweek', 'pe2019understanding', 'pe2019designing', 'hackweektoolkit', 'wocschedule', 'intIdeation', 'intTeam', 'hackweektoolkitrecruit', 'sheinnovates', 'entrofy'];
+var preparationReferences = ['intTheme', 'intGoal', 'intTeam', 'intMentor', 'nolte2020mentor', 'codecademy', 'intRecruitment', 'woc', 'astrohackweek', 'geohackweek', 'hackhpc', 'brainhack'];
+var durationReferences = ['intRecruitment', 'intMentor', 'intStakeholder', 'intTheme', 'g48how', 'oneweek', 'chihacknight', 'intGoal', 'wocschedule', 'intTeam'];
+var ideationReferences = ['filippova2017from', 'intStakeholder', 'intTheme', 'dayofcivichacking', 'greenhackathon', 'nolte2019touched', 'intGoal', 'pe2019understanding', 'intContinuity', 'flow', 'brainstorm', 'intMentor', 'brainstormquestion', 'intTeam', 'intPreparation'];
+var teamReferences = ['intRecruitment', 'intIdeation', 'intStakeholder', 'pe2020corporate', 'hackbox', 'pe2019understanding', 'steelhacks', 'intAgenda', 'intCompetition', 'intGoal', 'nolte2020mentor', 'intMentor', 'intTheme'];
+var agendaReferences = ['intStakeholder', 'intMentor', 'intTheme', 'intGoal', 'intRecruitment', 'intCompetition', 'woctutorial', 'intIdeation', 'intTeam', 'sgci2019', 'g48cyber', 'intPreparation', 'brainhack', 'icebreaker', 'nofun', 'intDuration', 'nolte2020mentor'];
+var mentoringReferences = ['intContinuity', 'intAgenda', 'intIdeation', 'nolte2020mentor', 'intPreparation', 'greatmentor', 'perfectmentor', 'g48mentor', 'intCompetition'];
+var continuityReferences = ['intGoal', 'intStakeholder', 'startupsfromhackathons', 'intAgenda', 'intIdeation', 'pe2020corporate', 'nolte2018you', 'intPreparation', 'medina2019does', 'intTeam'];
+
+function printReference(decision, thisId) {
+	var decisionReferences = getDecisionReferences(decision);
+	var decisionNo = getDecisionNo(decision);
+	
+	for (var i = 0; i < decisionReferences.length; i++) {
+		var id = decisionReferences[i];
+		for (var j = 0; j < references.length; j++) {
+			var reference = references[j];
+			if (id == reference['id']) {
+				if (reference['id'] == thisId) {
+					document.write('<a href="#decisionsModal'+decisionNo+reference['id']+'">'+(i+1)+'</a>');
+				}
+			}
+		}
+	}
+};
+
+function printReferenceList(decision) {
+	var decisionReferences = getDecisionReferences(decision);
+	var decisionNo = getDecisionNo(decision);
+
+	for (var i = 0; i < decisionReferences.length; i++) {
+		var id = decisionReferences[i];
+		for (var j = 0; j < references.length; j++) {
+			var reference = references[j];
+			if (id == reference['id']) {
+				if (reference['type'] == 'internal') {
+					document.write('<li id="decisionsModal'+decisionNo+reference['id']+'" class="list-item">Decision <a data-dismiss="modal" data-toggle="modal" href="#decisionsModal'+reference['link']+'">'+reference['text']+'</a></li>');
+				} else if (reference['type'] == 'external') {
+					document.write('<li id="decisionsModal'+decisionNo+reference['id']+'" class="list-item"><a href="'+reference['link']+'" target="_blank">'+reference['text']+'</a></li>');
+				} else if (reference['type'] == 'pub') {
+					document.write('<li id="decisionsModal'+decisionNo+reference['id']+'" class="list-item">'+reference['text']+' (<a href="files/'+reference['link']+'" target="_blank">pdf</a>)</li>');
+				} else if (reference['type'] == 'pubnopdf') {
+					document.write('<li id="decisionsModal'+decisionNo+reference['id']+'" class="list-item">'+reference['text']+'</li>');
+				}
+			}
+		}
+	}
+};
+
+function getDecisionReferences(decision) {
+	if (decision == 'goal') {
+		return goalReferences;
+	} else if (decision == 'theme') {
+		return themeReferences;
+	} else if (decision == 'competition') {
+		return competitionReferences;
+	} else if (decision == 'stakeholder') {
+		return stakeholderReferences;
+	} else if (decision == 'recruitment') {
+		return recruitmentReferences;
+	} else if (decision == 'preparation') {
+		return preparationReferences;
+	} else if (decision == 'duration') {
+		return durationReferences;
+	} else if (decision == 'ideation') {
+		return ideationReferences;
+	} else if (decision == 'team') {
+		return teamReferences;
+	} else if (decision == 'agenda') {
+		return agendaReferences;
+	} else if (decision == 'mentor') {
+		return mentoringReferences;
+	} else if (decision == 'continuity') {
+		return continuityReferences;
+	}
+};
+
+function getDecisionNo(decision) {
+	if (decision == 'goal') {
+		return 1;
+	} else if (decision == 'theme') {
+		return 2;
+	} else if (decision == 'competition') {
+		return 3;
+	} else if (decision == 'stakeholder') {
+		return 4;
+	} else if (decision == 'recruitment') {
+		return 5;
+	} else if (decision == 'preparation') {
+		return 6;
+	} else if (decision == 'duration') {
+		return 7;
+	} else if (decision == 'ideation') {
+		return 8;
+	} else if (decision == 'team') {
+		return 9;
+	} else if (decision == 'agenda') {
+		return 10;
+	} else if (decision == 'mentor') {
+		return 11;
+	} else if (decision == 'continuity') {
+		return 12;
+	}
+};
